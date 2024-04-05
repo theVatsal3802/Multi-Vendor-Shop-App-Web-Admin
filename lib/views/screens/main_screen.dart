fda3e90e6dc8d28676d88ced9e76a20d487a9b80
Vatsal Adhiya <vatsaladhiya248@gmail.com>
2024-04-05 15:19:29 +0530

    Started UI Development

---
+++ lib/views/screens/main_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_admin_scaffold/admin_scaffold.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return AdminScaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Management",
+          textScaler: TextScaler.noScaling,
+        ),
+      ),
+      body: const Text("data"),
+    );
+  }
+}
